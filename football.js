ab629fa9164e11321323d8e9c44081e8b2907765
kjsguld <kjsguld@gmail.com>
2016-11-01 11:36:49 +0100

    new data source
    Crowdscores.com

--- football.js
+++ football.js
@@ -5,14 +5,23 @@ angular.module('football', [])
     var vm = this;
     vm.list = [];
 
-    //Simple GET request
-    $http({
+    // football-data.org GET request
+    // var request = {
+    //   method: 'GET',
+    //   url: 'http://api.football-data.org/v1/competitions/398/leagueTable?league=CL',
+    //   headers: {
+    //     'X-Auth-Token' : 'd0435abf7a0046b0abcba2871bc6df51'
+    //   }
+    // }
+
+    // crowdscores.com GET request
+    var request = {
       method: 'GET',
-      url: 'http://api.football-data.org/v1/competitions/398/leagueTable',
-      headers: {
-        'X-Auth-Token' : 'd0435abf7a0046b0abcba2871bc6df51'
-      }
-    }).then(function successCallback(response) {
+      url: 'https://api.crowdscores.com/v1/league-tables?competition_id=42&api_key=dcb6392b40ca452aaca1ee4d8258857b'
+
+    }
+
+    $http(request).then(function successCallback(response) {
         // this callback will be called asynchronously
         // when the response is available
         vm.list = response.data;
